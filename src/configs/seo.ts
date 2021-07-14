570cd922705334802bc141f81fab23a43ffb32dd
Lee GyeongJun <cockyb@nate.com>
2021-07-14 13:05:32 +0900

    Feature: set default SEO

---
+++ src/configs/seo.ts
@@ -0,0 +1,30 @@
+import siteMetadata from 'data/siteMetadata.json';
+
+export const SEO = {
+  title: siteMetadata.title,
+  description: siteMetadata.description,
+  openGraph: {
+    type: 'website',
+    locale: siteMetadata.language,
+    url: siteMetadata.siteUrl,
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    images: [
+      {
+        url: siteMetadata.socialBanner,
+        alt: siteMetadata.title,
+        width: 1200,
+        height: 600,
+      },
+    ],
+  },
+  twitter: {
+    cardType: 'summary_large_image',
+  },
+  additionalMetaTags: [
+    {
+      name: 'author',
+      content: siteMetadata.author,
+    },
+  ],
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
+import {DefaultSeo} from 'next-seo';
+
+import {SEO} from 'configs/seo';
+
 import 'styles/globals.scss';
 import 'tailwindcss/tailwind.css';
 
 import type {AppProps} from 'next/app';
 
-function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp;
+
